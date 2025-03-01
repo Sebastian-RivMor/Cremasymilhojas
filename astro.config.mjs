5b90fb524fe533acd025b356309b8e795f874df2
Sebastian-RivMor <117615532+Sebastian-RivMor@users.noreply.github.com>
2025-02-28 19:39:06 -0500

    actualizado ultimo 28/02

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import auth from "auth-astro";
+import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [auth()],
+  adapter: vercel(),
 });
